85331f4e5dacb9fdcb8fb1fa666e3a3265d181fd
alex-brooklyn <alex.vukovity@gmail.com>
2018-12-09 18:03:22 +0100

    Palindrome: Improve method documentation

--- palindrome.js
+++ palindrome.js
@@ -32,17 +32,19 @@ function palindrome(string) {
 
   /**
    * Again you have to convert string into an array to be able to use every()
-   * char represents each character of the string during iteration. i represents array index
-   * When you compare it you have to compare string's current char to its mirror char on the other
-   * side of the string
-   * This mirror char you would get by getting the strings length subtracting the current i and
-   * then -1 because length starts counting from 1 and index from 0
+   * character parameter represents each character of the string during iteration. index represents
+   * an array index of an array element and starts counting from 0
+   * To get the boolean value true or false whether its palindrome or not you have to compare
+   * the string's current character in every given iteration to its
+   * mirror character on the other side of the string so to say if they are the same
+   * This mirror character you would get by getting the strings length and subtracting the current
+   * index and then -1 because length starts counting from 1 and array index from 0
    * This -1 thing has nothing to do with palindrome its just JavaScript thing that .length starts
-   * counting from 0 and index of an element of an array starts from 0 and to use them together
+   * counting from 1 and index of an element of an array starts from 0 and to use them together
    * you have to do this -1 thing so it brings them to the same level so to say, otherwise
    * length will always be higher by 1
    */
-  return string.split('').every((char, i) => {
-    return char === string[string.length - i -1]
+  return string.split('').every((character, index) => {
+    return character === string[string.length - index -1]
   })
 }
